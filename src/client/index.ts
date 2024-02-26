42f413e042782d85b42135f8389c2cd02bfa15a3
T1B0 <t1b0.gm@gmail.com>
2024-02-26 15:11:34 +0100

    fix: simplify top level exports

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,3 +2,4 @@
 export { Client, SimpleClient, SingleClient } from './client.js'
 export { TcpClient, TcpOption } from './tcp.client.js';
 export { TlsClient, TlsOption } from './tls.client.js';
+export * from './client.utils.js';
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 
 
-import { PollingStrategy } from "./wire/message/poll.utils.js";
-import { ConsumerKind } from "./wire/offset/offset.utils.js";
-import { Partitioning } from './wire/message/partitioning.utils.js';
-import { HeaderValue } from './wire/message/header.type.js';
+export { PollingStrategy } from "./wire/message/poll.utils.js";
+export { ConsumerKind } from "./wire/offset/offset.utils.js";
+export { Partitioning } from './wire/message/partitioning.utils.js';
+export { HeaderValue } from './wire/message/header.type.js';
 
 export * from './client/index.js'
 export { ClientProvider, RawClient, CommandResponse } from './client/client.type.js';
 export { Id } from './wire/identifier.utils.js'
-
-export const helpers = {
-  PollingStrategy,
-  ConsumerKind,
-  Partitioning,
-  HeaderValue
-};
