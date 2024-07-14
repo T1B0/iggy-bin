596367bd8bd5530a72f160ba2a59bfab3241db43
T1B0 <t1b0.gm@gmail.com>
2024-07-14 18:30:14 +0200

    refactor: extract socket wrapping to its own files

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,3 +3,4 @@ export { Client, SimpleClient, SingleClient } from './client.js'
 export { TcpClient, type TcpOption } from './tcp.client.js';
 export { TlsClient, type TlsOption } from './tls.client.js';
 export * from './client.utils.js';
+export * from './client.socket.js';
